b5be5f6b0cd4830f46ee0763276e7d09dc2392e3
Marek Szkudelski <marek.szkudelski@globegroup.pl>
2019-01-15 13:55:11 +0100

    some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export const animation = trigger('routeAnimations', [
       style({
         position: 'absolute',
         top: 0,
+        bottom: 0,
         left: 0,
         width: '100%',
       }),
