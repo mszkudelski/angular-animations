c71edc930911d304e80524685090b63e14a4bd58
Marek Szkudelski <marek.szkudelski@globegroup.pl>
2019-01-15 15:32:43 +0100

    providing users

---
+++ src/app/view1/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+@Injectable()
+export class UsersService {
+  getUsers(): Observable<object[]> {
+    return of([
+      {
+        id: 123,
+        name: 'User user',
+        age: 23,
+        description: 'Lorem ipsum',
+        avatar: 'https://picsum.photos/50',
+      },
+      {
+        id: 123,
+        name: 'User user',
+        age: 23,
+        description: 'Lorem ipsum',
+        avatar: 'https://picsum.photos/50',
+      },
+      {
+        id: 123,
+        name: 'User user',
+        age: 23,
+        description: 'Lorem ipsum',
+        avatar: 'https://picsum.photos/50',
+      },
+    ]).pipe(delay(1500));
+  }
+}
--- src/app/view1/view1.component.ts
+++ src/app/view1/view1.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UsersService } from './users.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-view1',
   templateUrl: './view1.component.html',
-  styleUrls: ['./view1.component.css']
+  styleUrls: ['./view1.component.css'],
+  providers: [UsersService],
 })
-export class View1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class View1Component {
+  users: Observable<object[]>;
+  constructor(private _usersService: UsersService) {
+    this.users = this._usersService.getUsers();
   }
-
 }
