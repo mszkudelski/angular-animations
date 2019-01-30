f2191919d95bb7eb4a54c5b57a1ce3a9a45172fd
Marek Szkudelski <marek.szkudelski@globegroup.pl>
2019-01-30 14:32:59 +0100

    simple example

--- src/app/view2/view2.component.ts
+++ src/app/view2/view2.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-view2',
   templateUrl: './view2.component.html',
   styleUrls: ['./view2.component.scss'],
+  animations: [
+    trigger('move', [
+      transition('* => *', [
+        animate('500ms ease-in-out', style({ transform: 'translateX(50px)' })),
+        animate('500ms ease-in-out', style({ transform: 'translateX(0)' })),
+      ]),
+    ]),
+  ],
 })
-export class View2Component implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class View2Component {
+  timer = timer(1000, 1000);
 }
